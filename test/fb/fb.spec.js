185da209f310e5ac6ded5fb278dbfafbf82808b1
Sergei Liski <sergeiliski@gmail.com>
2021-05-18 16:22:10 +0100

    Remove test

--- test/fb/fb.spec.js
+++ test/fb/fb.spec.js
@@ -7,10 +7,3 @@ test('Facebook getMessages() returns not null', async () => {
   const messages = await fb.getMessages()
   expect(messages).not.toBeNull()
 });
-
-test('Facebook getComment() returns not null', async () => {
-  const pages = Helper.getFacebookPages()
-  const fb = new Facebook({ pages: pages})
-  const messages = await fb.getComment(pages[0], '230482852179178_230488138845316')
-  expect(messages).not.toBeNull()
-});
